e250111d0f7c93ec2375ecaa5e26e48cb545d8c2
old2son <caiye2531889700@gmail.com>
2023-03-01 00:33:03 +0800

    update sass

--- mock/api/indexMusicRec.js
+++ mock/api/indexMusicRec.js
@@ -9,21 +9,21 @@ export default [
                 data: {
                     timeRec: [
                         {
-                            pic: "http://192.168.0.212:81/src/assets/temp_003.jpg",
+                            pic: "/src/assets/temp_003.jpg",
                             text: "宇宙榜"
                         },
                         {
-                            pic: "http://192.168.0.212:81/src/assets/temp_003.jpg",
+                            pic: "/src/assets/temp_003.jpg",
                             text: "银河系榜"
                         }
                     ],
                     recentList: [
                         {
-                            pic: "http://192.168.0.212:81/src/assets/temp_004.jpg",
+                            pic: "/src/assets/temp_004.jpg",
                             text: "流行乐"
                         },
                         {
-                            pic: "http://192.168.0.212:81/src/assets/temp_004.jpg",
+                            pic: "/src/assets/temp_004.jpg",
                             text: "vtb专辑"
                         }
                     ]
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router/index.js';
-// import '@/style/style.css';
+// import '@/styles/style.scss';
 
 const app = createApp(App);
 app.use(router);
